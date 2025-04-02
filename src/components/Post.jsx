97debe1440341807d331d4ab4bb0e0645a94e1f9
Thiago <thiagodeboliveira@outlook.com>
2025-04-02 00:37:16 -0300

    adding the first component post

---
+++ src/components/Post.jsx
@@ -0,0 +1,12 @@
+function Post(){
+    return (
+        <div>
+            <p>Thiago Oliveira</p>
+            <p>im going to be a billionaire</p>
+        </div>
+    );
+}
+
+
+
+export default Post;
