c3b532cf57128afa7f73747e794632bed6b1569d
Thiago <thiagodeboliveira@outlook.com>
2025-04-09 10:35:43 -0300

    exploring state management and conditional content by creating a header and improving the modal logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,31 @@
+import { useState } from "react"
+import MainHeader from "./components/MainHeader"
 import PostList from "./components/PostList"
 
 
 function App() {
-  
+  const [modalIsVisible, setModalIsVisible] = useState(false);
+
+  function hideModalHandler(){
+    setModalIsVisible(false);
+}
+  function showModalHandler(){
+    setModalIsVisible(true);
+  }
+
 
   return (
+
+    <>
     <main>
-      <PostList />
+      <MainHeader onCreatePost={showModalHandler}/>
+      <PostList isPosting={modalIsVisible}
+      onStopPosting={hideModalHandler}
+      />
       
     </main>
+    </>
+    
   )
 }
 
--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -5,11 +5,17 @@ import classes from './PostList.module.css';
 import Modal from './Modal';
 
 
-function PostList(){ 
+function PostList({isPosting, onStopPosting}){ 
+
+    const [modalIsVisible, setModalIsVisible] = useState(true);
 
     const [enteredBody, setEnteredBody] = useState('');
 
     const [enteredAuthor, setEnteredAuthor] = useState('');
+
+    function hideModalHandler(){
+        setModalIsVisible(false);
+    }
 
     function bodyChangeHandler(event){
         setEnteredBody(event.target.value);
@@ -21,12 +27,15 @@ function PostList(){
     
     return(
         <>
-            <Modal>
+            {isPosting ? (
+                <Modal onClose={onStopPosting}>
+            
                 <NewPost
                 onBodyChange= {bodyChangeHandler}
                 onAuthorChange= {onAuthorChange}
                 />
             </Modal>
+            ) : null}
         <ul className={classes.posts}>
             <Post author={enteredAuthor} body = {enteredBody}/>
             <Post author="John" body = "For God so loved the world that he gave his one and only Son, that whoever believes in him shall not perish but have eternal life. - John 3:16"/>
