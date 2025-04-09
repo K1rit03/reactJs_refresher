d59209b688081a69bb94e8effba2a93c65bf5c96
Thiago <thiagodeboliveira@outlook.com>
2025-04-09 11:06:51 -0300

    handling form submission

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -8,36 +8,20 @@ import Modal from './Modal';
 function PostList({isPosting, onStopPosting}){ 
 
     const [modalIsVisible, setModalIsVisible] = useState(true);
-
-    const [enteredBody, setEnteredBody] = useState('');
-
-    const [enteredAuthor, setEnteredAuthor] = useState('');
-
+    
     function hideModalHandler(){
         setModalIsVisible(false);
     }
-
-    function bodyChangeHandler(event){
-        setEnteredBody(event.target.value);
-    }
-
-    function onAuthorChange(event){
-        setEnteredAuthor(event.target.value);
-    }
-    
     return(
         <>
-            {isPosting ? (
+            {isPosting &&  (
                 <Modal onClose={onStopPosting}>
-            
-                <NewPost
-                onBodyChange= {bodyChangeHandler}
-                onAuthorChange= {onAuthorChange}
+                    <NewPost onCancel={onStopPosting}
                 />
             </Modal>
-            ) : null}
+            )}
         <ul className={classes.posts}>
-            <Post author={enteredAuthor} body = {enteredBody}/>
+           
             <Post author="John" body = "For God so loved the world that he gave his one and only Son, that whoever believes in him shall not perish but have eternal life. - John 3:16"/>
         </ul>
         </>
